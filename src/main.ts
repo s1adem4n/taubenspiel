66f9f45c8bd113bbc223b5afe3aa025b6dc5cbbf
s1adem4n <68190441+s1adem4n@users.noreply.github.com>
2025-03-26 11:55:49 +0000

    flappy

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './style.css';
+import "./styles.css";
 import kaplay from 'kaplay';
 
 const k = kaplay({
@@ -14,13 +14,52 @@ const k = kaplay({
 });
 
 k.scene('game', () => {
-  k.add([
-    k.rect(k.width(), 16),
-    k.pos(0, 0),
-    k.anchor('bot'),
-    k.area(),
-    k.body({ isStatic: true }),
-  ]);
+  let PIPE_OPEN = 80;
+  const PIPE_MIN = 40;
+  let SPEED = 150;
+  let score = 0;
+
+  function spawnPipe() {
+    score += 1;
+    const topHeight = k.rand(PIPE_MIN, PIPE_OPEN - PIPE_MIN);
+    const bottomHeight = k.height() - PIPE_OPEN - topHeight;
+
+    k.add([
+      k.pos(k.width(), 0),
+      k.rect(64, topHeight),
+      k.color(0, 127, 255),
+      k.area(),
+      k.move(k.LEFT, SPEED),
+      k.offscreen({ destroy: true }),
+      'pipe',
+    ]);
+    k.add([
+      k.pos(k.width(), topHeight + PIPE_OPEN),
+      k.rect(64, bottomHeight),
+      k.color(0, 127, 255),
+      k.area(),
+      k.move(k.LEFT, SPEED),
+      k.offscreen({ destroy: true }),
+      'pipe',
+    ]);
+    k.add([
+      k.text(score.toString()),
+      k.pos(k.width() + 30, k.height() - 20),
+      k.anchor('center'),
+      k.color(255, 255, 255),
+      k.move(k.LEFT, SPEED),
+      k.offscreen({ destroy: true }),
+    ]);
+  }
+
+  k.loop(1, () => {
+    spawnPipe();
+    if (PIPE_OPEN > PIPE_MIN) {
+      PIPE_OPEN -= 1;
+      SPEED += 5;
+    }
+  });
+
   k.add([
     k.rect(k.width(), 16),
     k.pos(0, k.height() - 16),
@@ -29,13 +68,37 @@ k.scene('game', () => {
     k.color(127, 200, 255),
   ]);
 
-  k.setGravity(200);
+  k.setGravity(400);
 
   const player = k.add([k.rect(20, 20), k.pos(100, 100), k.area(), k.body()]);
-  player.move(0, 100);
+
+  player.onCollide('pipe', () => {
+    k.go('gameOver');
+  });
+
+  player.onUpdate(() => {
+    if (player.pos.y <= 0) {
+      k.go('gameOver');
+    }
+  });
 
   k.onButtonDown('jump', () => {
-    player.jump(150);
+    player.jump(100);
+  });
+});
+
+k.scene('gameOver', () => {
+  k.add([
+    k.text('Du bist vol kage hahah'),
+    k.color(255, 0, 0),
+    k.pos(k.width() / 2, k.height() / 2),
+    k.anchor('center'),
+  ]);
+  k.onClick(() => {
+    k.go('game');
+  });
+  k.onButtonDown('jump', () => {
+    k.go('game');
   });
 });
 
